27e24bc2ce300cff48bdeb1574a5105cd8f3ba87
swith <swith@tetrasis.ru>
2018-03-10 13:13:38 +0200

    ruby ver specified

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.10.1"
 set :application, "RecrApp"
 set :repo_url, "git@github.com:swith/RecrApp.git"
 set :deploy_to, '/home/deploy/RecrApp'
-
+set :rbenv_ruby, '2.5.0'
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 # Default branch is :master
