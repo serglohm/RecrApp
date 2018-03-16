bac7290e81875afbbb9d850bb226ffdcb5d61be6
swith <swith@tetrasis.ru>
2018-03-16 09:29:02 +0200

    added enum statuses

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,6 @@
 class Candidate < ApplicationRecord
   belongs_to :user
   belongs_to :source
+  enum status: [:in_progress, :hired, :rejected, :withdrawn]
   mount_uploader :resume, ResumeUploader
 end
