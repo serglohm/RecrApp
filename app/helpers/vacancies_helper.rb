03b78416835776e2d71246ec6d0f0987c247a7d8
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-03-29 00:12:03 +0300

    added font to the cell vacancy index

--- app/helpers/vacancies_helper.rb
+++ app/helpers/vacancies_helper.rb
@@ -1,6 +1,6 @@
 module VacanciesHelper
-  def check_tg(link)
-    if link.present?
+  def check_completion(field)
+    if field.present?
       content_tag(:td, fa_icon('check'))
     else
       content_tag(:td, fa_icon('times'), class:'table-danger')
