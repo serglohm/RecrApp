93442b338fd989b0d95a03d6a87b3efa2456e004
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-04-17 17:26:45 +0300

    dashboard updated

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @last_candidates = current_user.candidates
-                                   .where.not(status: :hired)
+                                   .where(status: :in_progress)
                                    .order(updated_at: :desc)
                                    .last(10)
   end
