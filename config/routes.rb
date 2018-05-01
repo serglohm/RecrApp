c968948a25a0be6fee8c4d10670bd9f6664148a7
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-05-01 12:44:43 +0300

    some indications

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
     match :set_hired, to: 'assignments#set_hired', via: :patch
     match :set_rejected, to: 'assignments#set_rejected', via: :patch
     match :set_withdrawn, to: 'assignments#set_withdrawn', via: :patch
-    get '/hire_modal', to: 'assignments#hire_modal', as: 'hire_modal'
-    get '/reject_modal', to: 'assignments#reject_modal', as: 'reject_modal'
-    get '/withdrawn_modal', to: 'assignments#withdrawn_modal', as: 'withdrawn_modal'
+    get '/hire', to: 'assignments#hire_modal', as: 'hire_modal'
+    get '/reject', to: 'assignments#reject_modal', as: 'reject_modal'
+    get '/withdrawn', to: 'assignments#withdrawn_modal', as: 'withdrawn_modal'
   end
   devise_for :users
   resources :users
