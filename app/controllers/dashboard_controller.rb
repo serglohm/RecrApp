3f9e222a0851e377f12dd473ff9048f990ba117c
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-05-02 16:38:18 +0300

    fixed dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
   def index
     @last_candidates = current_user.candidates
                                    .where(status: :in_progress)
-                                   .order(updated_at: :desc)
-                                   .last(10)
+                                   .order(updated_at: :asc)
+                                   .first(10)
     @upcoming_events = current_user.events.active.upcoming.order(scheduled_on: :asc)
   end
 end
