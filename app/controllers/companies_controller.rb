7c35499f1cd20212841c2e7ccf7cc1a3403b7091
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-06-15 13:32:26 +0300

    modified company edit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -29,7 +29,7 @@ class CompaniesController < ApplicationController
 
     respond_to do |format|
       if @company.save
-        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.html { redirect_to edit_company_path, notice: 'Company was successfully created.' }
         format.json { render :show, status: :created, location: @company }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class CompaniesController < ApplicationController
   def update
     respond_to do |format|
       if @company.update(company_params)
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.html { redirect_to companies_path, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit }
