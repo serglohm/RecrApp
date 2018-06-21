500a562d5b3a2d49b5f629e615f27075ecd21d02
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-06-21 13:22:12 +0300

    fixed path env

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+env :PATH, ENV['PATH']
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -5,7 +6,7 @@
 
 # Example:
 #
-# set :output, "/path/to/my/cron_log.log"
+set :output, "log/crontab.log"
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
